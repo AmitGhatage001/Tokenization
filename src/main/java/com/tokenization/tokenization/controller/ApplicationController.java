7b389f1cdedc45f021a98cf1c5d6333682255421
Amit Ghatage <amitghatage001@gmail.com>
2024-11-25 17:14:02 +0530

    implemented StringTokenizer in Api

--- src/main/java/com/tokenization/tokenization/controller/ApplicationController.java
+++ src/main/java/com/tokenization/tokenization/controller/ApplicationController.java
@@ -3,11 +3,22 @@ package com.tokenization.tokenization.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 @RestController
 public class ApplicationController {
 
+
+
     @GetMapping("get")
-    public String getData(){
-        return "Got response";
+    public ArrayList<String> getData(){
+        ArrayList<String> list = new ArrayList<>();
+        StringTokenizer str = new StringTokenizer("my name is khan"," ");
+        while (str.hasMoreTokens()) {
+            list.add(str.nextToken());
+//            System.out.println(str.nextToken());
+        }
+        return list;
     }
 }
